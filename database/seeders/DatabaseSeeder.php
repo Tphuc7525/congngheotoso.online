d25a96e57d5b42b8d00ccb0782d2c96e2adbde92
Tphuc7525 <thienphuc7525@gmail.com>
2025-04-20 10:21:11 +0700

    day1 60 phút

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,25 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
-{
+
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
     {
         // \App\Models\User::factory(10)->create();
+        public function run(): void
+        {
+            DB::table('users')->insert([
+                'name'=>'Newbie Laravel',
+                'email'=>'thienphuc7525@gmail.com',
+                'password'=>Hash::make('password'),
+            ]);
+        }
     }
-}
+
