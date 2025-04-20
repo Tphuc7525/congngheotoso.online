0d9d6b5f8353f6e45c334ba0ef2eca0af2fef398
Tphuc7525 <thienphuc7525@gmail.com>
2025-04-20 10:58:57 +0700

    20/4/2025 app, web

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use App\Http\Controllers\Backend\AuthController;
 use Illuminate\Support\Facades\Route;
+Route::get('/', function(){
+    return view('welcome');
+});
 
-Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::get('admin', [AuthController::class, 'index'])->name('auth.admin');
+Route::post('login',[AutherController::class,'login'])->name('auth.login');
